26b9b8606d87f69c9346cc1d13afc3e04583ee61
Vrynzz <yingzz@gmail.com>
2024-10-28 03:57:33 +0800

    Minor Edits
    Minor Edits

--- pages/7_Methodology.py
+++ pages/7_Methodology.py
@@ -9,6 +9,6 @@ st.set_page_config(
 
 st.title("Methodology")
 
-st.image('data/Chatbot_1.png')
+st.image('../data/Chatbot_1.png')
 
-st.image('data/Chatbot_2.png')
+st.image('../data/Chatbot_2.png')
